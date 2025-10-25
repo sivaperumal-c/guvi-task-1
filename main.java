f44e4d23a184e4f3006016fb1082bdeb7a55636a
sivaperumal-c <sivaperumal1278@gmail.com>
2025-10-25 12:02:47 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,55 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        //int number = 10;
+
+//        if (number>10){
+//
+//            System.out.println("win");
+//        }
+//        else if(number<=19){
+//            System.out.println("retry");//
+//        }
+//        else {
+//             System.out.println("failed");
+
+//        int day = 7;
+//        if (day == 1) {
+//
+//            System.out.println("monday");
+//        } else if (day == 2) {
+//            System.out.println("Tuesday");
+//        } else if (day==3) {
+//            System.out.println("Wednesday");
+//        } else if (day==4) {
+//            System.out.println("Thursday");
+//        } else if (day==5) {
+//            System.out.println("friday");
+//        } else if (day==6) {
+//            System.out.println("Saturday");
+//
+//        } else if (day==7) {
+//            System.out.println("Sunday");
+//        } else {
+//            System.out.println("failed");
+        //}
+
+
+        //WAP TO CHECK PERSON ELIGIBILITY
+        int age = 26;
+        boolean voteidavailable =true;
+
+        if (age >= 18 && voteidavailable) {
+                System.out.println("eligible for vote" );
+            }else {
+            System.out.println("not eligible for vote");
+        }
+
+    }
+}
+
+
